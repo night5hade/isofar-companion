682b53d5bade4d87a2df82f4f624f047e34cb9d8
night5hade <129759458+night5hade@users.noreply.github.com>
2024-11-23 22:14:59 -0500

    Updated CH3 and CH4

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1731953439|54649176';
+const CACHE_VERSION = '1732418015|99590067';
 /** @type {string} */
 const CACHE_PREFIX = 'Isofar Companion-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
